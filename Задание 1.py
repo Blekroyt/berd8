ec36e6f1303644fc516dbb2083280d643128fc50
Blekroyt <99321528+Blekroyt@users.noreply.github.com>
2022-06-14 17:39:42 +0300

    Update Задание 1.py

--- Задание 1.py
+++ Задание 1.py
@@ -1,1 +1,22 @@
+#!/usr/bin/env python3
+# -*- кодировка: utf-8 -*-
+import math
+import sys
 
+def test():
+  number = int(input('Введите число: '))
+  if number > 0:
+    positive()
+  elif number < 0:
+    negative()
+  else:
+    print('Я вас не совсем понял. ;)')
+    test()
+ 
+def positive():
+  print('Положительное')
+ 
+def negative():
+  print('Отрицательное')
+ 
+test()
