176082c8c0acdb508166229c1b812b06fc966449
Blekroyt <99321528+Blekroyt@users.noreply.github.com>
2022-06-14 17:38:31 +0300

    Update Задание 2.py

--- Задание 2.py
+++ Задание 2.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python3
+# -*- кодировка: utf-8 -*-
+import math
+import sys
 
+def cylinder(r, h):
+from math import pi
+def circle(r): return pi*r**2
+s = 2*pi*r*h
+if input('Full area? [y/n]: ') == 'y': \
+s += 2*circle(r)
+return s
+
+r, h = 1, 1
+print('s =', cylinder(r, h))
