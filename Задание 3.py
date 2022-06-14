a2daf4ea93f789ca8eea6ad4c832f7cf59fd71d5
Blekroyt <99321528+Blekroyt@users.noreply.github.com>
2022-06-14 17:37:41 +0300

    Update Задание 3.py

--- Задание 3.py
+++ Задание 3.py
@@ -1,1 +1,29 @@
+#!/usr/bin/env python3
+# -*- кодировка: utf-8 -*-
+import math
+import 
 
+def getInput():
+    return input()
+ 
+ 
+def testInput(a):
+    if type(a) == int or type(a) == float:
+        return True
+    elif a.isnumeric():
+        return True
+    else:
+        return False
+ 
+ 
+def strToInt(a):
+    return int(a)
+ 
+ 
+def printInt(a):
+    print(a)
+ 
+ 
+b = getInput()
+if testInput(b):
+    printInt(strToInt(b))
